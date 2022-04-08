151e8be9b36e23856300fa3a54bf994eecee09c2
JoelCAI <joelargentina12345@gmail.com>
2022-04-08 16:51:49 -0300

    Agregar archivos de proyecto.

---
+++ Fechas22/Sistema.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fechas22
+{
+    public class Sistema
+    {
+        public void MenuPrincipal()
+        {
+
+            Validador.Bienvenida();
+
+            Validador.SoloFecha();
+
+            Validador.Despedida();
+        }
+
+        public void Iniciar()
+        {
+            MenuPrincipal();
+        }
+    }
+}
---
+++ Fechas22/Validador.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fechas22
+{
+    public class Validador
+    {
+        public static void Despedida()
+        {
+
+            Console.WriteLine("\n Gracias por usar nuestro Sistema presione cualquier teclar para finalizar");
+            Console.ReadKey();
+        }
+
+        public static void SoloFecha()
+        {
+
+            DateTime fecha = DateTime.Today;
+            Console.WriteLine(" \n La fecha de hoy es: " + fecha);
+
+        }
+
+
+        public static void Bienvenida()
+        {
+            Console.WriteLine("\n Bienvenido al Programa");
+        }
+    }
+}
